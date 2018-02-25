153ebc39672405234102b5055cd8de51a505cdcb
Andrey Kashapov <kashapovandrey@gmail.com>
2018-02-25 21:25:12 +0300

    additional_5 v.1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // config1 = [['(', ')']];
+  // ((()))()', config1), true);
+
+  var tmp = {};
+  var brackets = [];
+
+  if (str.length % 2) {
+    return false;
+    //console.log('false 1');
+  }
+  
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    tmp[bracketsConfig[i][0]] = bracketsConfig[i][1];    
+  }
+
+  //console.log(tmp);
+
+  str = str.split('');
+
+  //console.log(str);
+  
+  for (var i = 0; i < str.length; i++) {
+    //console.log(str[i]);
+    if (tmp[str[i]]) {
+      brackets.push(str[i]);
+    } else {
+      brackets.pop();
+    }
+
+    if (tmp[str[i]] == str[i]) {
+      brackets.pop();
+    }
+  }
+  
+  if (brackets.length >= 0)
+    return true;
+  else
+    return false;
+  
 }
